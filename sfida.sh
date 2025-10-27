d1cf3aabc4d1f6b131176533ae1e11a32907097b
Maurizio Paolini <maurizio.paolini@unicatt.it>
2025-10-27 18:57:08 +0100

    Using emulator with "sfida" in "settimana della scienza 2025"

---
+++ sfida.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# sfida lanciata in occasione della "settimana della scienza 2025" 21-27/10/2025
+
+testo="DNEZQ CIBCE GIUET VELKE MSLTD WMCTA UOWFX BCTGH IUNIQ GIPYH GMGLU MWZFN FCZJY RUHTJ WXETC I"
+
+./enigma_emulator --rotors I-V-II --rings BAR --pos TAC $testo
+
+
